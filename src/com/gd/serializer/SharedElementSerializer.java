93bb0711eb660d7b7237ad45ee4f7eb6886eebdd
zzhengjian <zzhengjian1612@gmail.com>
2016-09-09 03:02:35 -0700

    No need for sharedelement serializer
    No need for sharedelement serializer

--- src/com/gd/serializer/SharedElementSerializer.java
+++
@@ -1,8 +0,0 @@
-package com.gd.serializer;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class SharedElementSerializer {
-	public List<PageSerializer> pages = new ArrayList<PageSerializer>();
-}
